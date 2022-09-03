1680bac4d5ff0e2acce2a4b90f9ee87e37753674
AungPaingSoe <aungpaingsoe097238@gmail.com>
2022-09-03 20:22:55 +0630

    Many to Many

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -22,6 +23,28 @@ class HomeController extends Controller
     public function whoOwnPost($id){
         $post = Post::findOrFail($id);
         return $post->user;
+    }
+
+    public function getAllPostComments($id){
+        $post = post::findOrFail($id);
+        return $post->comments;
+    }
+
+    public function getAllTags($id){
+        $post = post::findOrFail($id);
+        return $post->tags;
+    }
+
+    public function createTags($id){
+        $post = post::findOrFail($id);
+        return $post->tags()->attach(['1','2']);
+    }
+
+    public function getAllPosts($id)
+    {
+        $tag = Tag::findOrFail($id);
+        return $tag->posts;
+
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,16 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
+// One To One
 Route::get('/profile/{id}',[HomeController::class,'profile']);
+
+// One To Many
 Route::get('/userOfposts/{id}',[HomeController::class,'posts']);
 Route::get('/whoOwnPost/{id}',[HomeController::class,'whoOwnPost']);
+Route::get('/getAllPostComments/{id}',[HomeController::class,'getAllPostComments']);
+
+// Many to Many
+Route::get('/getAllTagsWithPost/{id}',[HomeController::class,'getAllTags']);
+Route::get('/createTags/{id}',[HomeController::class,'createTags']);
+Route::get('/getAllPostsWithTag/{id}',[HomeController::class,'getAllPosts']);
 
